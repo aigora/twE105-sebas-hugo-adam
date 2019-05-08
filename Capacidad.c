15c6b74673a43cfe2dae45a33850151d2065bb25
hfranco47 <h.franco@alumnos.upm.es>
2019-05-08 12:40:46 +0200

    Update Capacidad.c

--- Capacidad.c
+++ Capacidad.c
@@ -3,6 +3,7 @@
   float aCapa (float Q, float V);
   float aVolt (float Q, float S); 
   float aCarg (float V, float S);
+  
   int main()
   {
   	char l;
@@ -15,21 +16,21 @@
   {
   case 'v':
   case 'V':
-    printf("valor de la carga almacenada y la capacidad del condensador?\n");
+    printf("Introduce el valor de la carga almacenada y la capacidad del condensador\n");
     scanf("%f %f", &Q ,&S);  
     valor = aVolt(Q,S);
     break;
     
 	case 'c':
 	case 'C':
-	printf("valor de la carga y el voltaje?\n");
+	printf("Introduce el valor de la carga y el voltaje\n");
     scanf("%f %f", &Q ,&V); 
 	valor = aCapa(Q,V);
 	break;
 	
 	case 'q':
 	case 'Q':
-	printf("valor de la Capacidad y el Voltaje?\n");
+	printf("Introduce el valor de la Capacidad y el Voltaje\n");
     scanf("%f %f", &V, &S); 
 	valor = aCarg(V,S);
 	break;	
@@ -40,15 +41,15 @@ return 0;
 
 float aVolt (float Q, float S)
 {
-    return Q/S ;
+    return Q/S;
 }
 
 float aCapa (float Q, float V)
 {
-    return Q/V ;
+    return Q/V;
 }
 
 float aCarg (float V, float S)
 {
-	return V*S ;
+	return V*S;
 }
