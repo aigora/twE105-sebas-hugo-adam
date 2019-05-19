bb6c595eb74f32ff0a6c445351322843f09f6f81
hfranco47 <47787868+hfranco47@users.noreply.github.com>
2019-05-19 12:01:57 +0200

    Update main.c

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ do{
 		    \n4) Corriente electrica y circuitos.\
 			\n5) Salir del programa.\n");
 	scanf ("%i", &a);
-	fprintf (f,"TEMA ELEGIDO: %i\n",a);
+	fprintf (f,"\nTEMA ELEGIDO: %i\n",a);
     switch (a)
 	{
 		case 1:
@@ -116,11 +116,11 @@ do{
 					
 				case 3:
 						system ("cls");
-					//CAMPO ELÉCTRICO PRODUCIDO POR DOS CARGAS EN UN PUNTO
+					//CAMPO ELÃ‰CTRICO PRODUCIDO POR DOS CARGAS EN UN PUNTO
 					printf ("CAMPO ELECTRICO PRODUCIDO POR DOS CARGAS EN UN PUNTO\n");
 	                printf ("Esribe donde estan situadas las cargas q1 y q2, y el punto donde actua el campo, respectivamente:\n");
 	                scanf ("%f %f %f %f %f %f", &q1.x, &q1.y, &q2.x, &q2.y, &p.x, &p.y);
-	                //Campo eléctrico producido por la carga 1:
+	                //Campo elÃ©ctrico producido por la carga 1:
 	                printf ("\nCAMPO ELECTRICO QUE PRODUCE LA PRIMERA CARGA\n");
 	                dist1 = distancia1(q1,p);
 	                printf ("Las componentes i y j del vector son, respectivamente: \n");
@@ -130,7 +130,7 @@ do{
 	                scanf ("%f", &Q1);
 	                printf ("El modulo del campo electrico es:\n");
 	                campo1(q1,p,Q1);
-	                //Campo eléctrico producido por la carga 2:
+	                //Campo elÃ©ctrico producido por la carga 2:
 	                printf ("\n\nCAMPO ELECTRICO QUE PRODUCE LA SEGUNDA CARGA\n");
 	                dist2 = distancia2(q2,p);
 	                printf ("Las componentes i y j del vector son, respectivamente: \n");
@@ -282,7 +282,7 @@ do{
 	                printf ("Introduzca donde esta situado el punto:\n");
 	                scanf ("%f %f", &p.x, &p.y);
 	                distancia(p,q,n); 
-	                //Energía que poduce cada carga en el punto y energía total
+	                //EnergÃ­a que poduce cada carga en el punto y energÃ­a total
 	                for (i=0; i<n; i++)
 	                {
 		                printf ("Introduzca el valor de la carga q%i\n", i+1);
